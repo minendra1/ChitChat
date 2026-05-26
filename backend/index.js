00b45547c0ad59b4e8612388da59aab841bcce4f
Minendra Gangwar <minendragangwar01@gmail.com>
2026-05-26 22:46:40 +0530

    feat: Integrate user and message routers; update server to use socket.io; add multer for file uploads

--- backend/index.js
+++ backend/index.js
@@ -5,23 +5,26 @@ import authRouter from "./routes/auth.routes.js"
 import cookieParser from "cookie-parser"
 dotenv.config()
 import cors from "cors"
+import userRouter from "./routes/user.routes.js"
+import messageRouter from "./routes/message.routes.js"
+import { app, server } from "./socket/socket.js"
+
+const port=process.env.PORT || 5000
 
 
-const port=process.env.PORT||5000
-
-const app=express()
-app.use(cors(
-    {origin:"http://localhost:5173",
-    credentials:true}
-));
+app.use(cors({
+    origin:"http://localhost:5173",
+    credentials:true
+}))
 app.use(express.json())
 app.use(cookieParser())
-
 app.use("/api/auth",authRouter)
+app.use("/api/user",userRouter)
+app.use("/api/message",messageRouter)
 
 
-app.listen(port,()=>{
+
+server.listen(port,()=>{
     connectDb()
     console.log("server started")
-    
 })
