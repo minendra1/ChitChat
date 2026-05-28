df4298b3cb088cbede9003120c008cadfd23341c
Minendra Gangwar <minendragangwar01@gmail.com>
2026-05-28 22:02:01 +0530

    refactor: Update custom hooks to remove unnecessary dependencies in useEffect; improve code clarity

--- frontend/src/customHooks/getMessages.jsx
+++ frontend/src/customHooks/getMessages.jsx
@@ -2,23 +2,26 @@ import axios from "axios"
 import { useEffect } from "react"
 import { serverUrl } from "../main"
 import { useDispatch, useSelector } from "react-redux"
-import { setOtherUsers, setUserData } from "../redux/userSlice"
 import { setMessages } from "../redux/messageSlice"
 
-const getMessage=()=>{
-    let dispatch=useDispatch()
-    let {userData,selectedUser}=useSelector(state=>state.user)
-    useEffect(()=>{
-        const fetchMessages=async ()=>{
+const getMessage = () => {
+    let dispatch = useDispatch()
+    let { selectedUser } = useSelector(state => state.user) 
+    
+    useEffect(() => {
+        const fetchMessages = async () => {
             try {
-                let result=await axios.get(`${serverUrl}/api/message/get/${selectedUser._id}`,{withCredentials:true})
+             
+                if (!selectedUser) return; 
+
+                let result = await axios.get(`${serverUrl}/api/message/get/${selectedUser._id}`, { withCredentials: true })
                 dispatch(setMessages(result.data))
             } catch (error) {
                 console.log(error)
             }
         }
         fetchMessages()
-    },[selectedUser,userData])
+    }, [selectedUser]) 
 }
 
 export default getMessage
