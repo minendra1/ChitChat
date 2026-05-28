2c18a9ffc1dece13c346730c35fa6721acbf250a
Minendra Gangwar <minendragangwar1@gmail.com>
2026-05-28 23:09:47 +0530

    Update CORS origin to production URL

--- backend/index.js
+++ backend/index.js
@@ -12,7 +12,7 @@ dotenv.config()
 const port = process.env.PORT || 5000
 
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "https://chitchat-1u0p.onrender.com",
     credentials: true
 }))
 app.use(express.json())
